106dffd9e3c9848aec280459c254c2af2a38b082
Guilhem64 <guilhem@DESKTOP-UAG98EP.localdomain>
2019-06-18 00:06:32 +0200

    first commit

---
+++ content/meta/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: "LegalBot", // <title>
+  shortSiteTitle: "LegalBot", // <title> ending for posts and pages
+  siteDescription: "Generator of bot",
+  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "Team Phoenix",
+  authorTwitterAccount: "Team Phoenix",
+
+  /* info */
+  headerTitle: "Team Phoenix",
+  headerSubTitle: "LegalBot",
+
+  /* manifest.json */
+  manifestName: "LegalBot",
+  manifestShortName: "LegalBot", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
